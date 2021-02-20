df9b8c48928f82b5b6241612483c02b626e53b69
roman.lelikov <roman.lelikov@brandmaker.com>
2021-02-20 21:25:17 +0700

    added remote browser config

--- src/test/java/settings/BaseTest.java
+++ src/test/java/settings/BaseTest.java
@@ -4,14 +4,25 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.open;
 
 public class BaseTest {
+
     @BeforeAll
     public static void config() {
-        Configuration.browserSize = "1920x1080";
         SelenideLogger.addListener("allure", new AllureSelenide());
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserSize = "1920x1080";
+
+        if (System.getProperty("remote_driver") != null) {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("enableVNC", true);
+            capabilities.setCapability("enableVideo", true);
+            Configuration.browserCapabilities = capabilities;
+            Configuration.remote = System.getProperty("remote_driver");
+        }
         open("https://demoqa.com");
     }
 }
