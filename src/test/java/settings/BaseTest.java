8eb1e35c499255d9393c0aa79bc5979cfa1f5f69
roman.lelikov <roman.lelikov@brandmaker.com>
2021-02-21 12:45:57 +0700

    added AttachmentsHelper added AfterEach

--- src/test/java/settings/BaseTest.java
+++ src/test/java/settings/BaseTest.java
@@ -3,10 +3,12 @@ package settings;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.open;
+import static pages.AttachmentsHelper.*;
 
 public class BaseTest {
 
@@ -24,5 +26,14 @@ public class BaseTest {
             Configuration.remote = System.getProperty("remote_driver");
         }
         open("https://demoqa.com");
+    }
+
+    @AfterEach
+    public void attachments() {
+        attachScreenshot("Last screenshot");
+        attachPageSource();
+        attachText("Browser console logs", getConsoleLogs());
+        if (System.getProperty("video_storage") != null)
+            attachVideo();
     }
 }
